c9a97bf0a8e3a45d205fdcebcc17077e0f57f4fa
Christopher Chaison <christopherc0825@gmail.com>
2020-08-09 19:19:20 -0400

    adding show edit and update to users controller for user profile editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,27 @@
 class UsersController < ApplicationController
     before_action :set_user!
 
+    def show
+    end
+
     def edit
     end
 
     def update
-    end
-
-    def destroy
+        if @user.update(user_params)
+            redirect_to @user
+        else
+            render 'edit'
+        end
     end
 
     private
 
     def set_user!
-        @user = current_user
+        @user = User.find_by(id: params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:email, :username, :password, :password_confirmation)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
   
+  resources :users, only: [:edit, :update, :show]
+  
   root to: "welcome#index"
 end
