67642228793588cff99d52f0e4f11a6c184ce665
Christopher Chaison <christopherc0825@gmail.com>
2020-08-09 19:46:28 -0400

    adding views and controller actions to breweries

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,11 +1,22 @@
 class BreweriesController < ApplicationController
+    before_action :set_brewery!, only: [:show, :edit, :update, :destroy]
+    
     def index
+        @breweries = Brewery.all
     end
     
     def new
+        @brewery = Brewery.new
     end
 
     def create
+        @brewery = Brewery.new(brewery_params)
+
+        if @brewery.save
+            redirect_to @brewery
+        else
+            render :new
+        end
     end
 
     def show
@@ -18,5 +29,15 @@ class BreweriesController < ApplicationController
     end
 
     def destroy
+    end
+
+    private
+
+    def brewery_params
+        params.require(:brewery).permit(:name, :location, :website)
+    end
+
+    def set_brewery!
+        @brewery = Brewery.find_by(id: params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
   
   resources :users, only: [:edit, :update, :show]
+  resources :breweries
   
   root to: "welcome#index"
 end
