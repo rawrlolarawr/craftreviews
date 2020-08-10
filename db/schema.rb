1f6d3ccc1e2b412634e26227ec480ebf5642baa2
Christopher Chaison <christopherc0825@gmail.com>
2020-08-10 14:15:42 -0400

    stubbing out reviews controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_180417) do
+ActiveRecord::Schema.define(version: 2020_08_10_180807) do
 
   create_table "beers", force: :cascade do |t|
     t.string "name"
     t.string "style"
     t.float "abv"
-    t.integer "brewery_id"
+    t.integer "brewery_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["brewery_id"], name: "index_beers_on_brewery_id"
   end
 
   create_table "breweries", force: :cascade do |t|
@@ -31,10 +32,12 @@ ActiveRecord::Schema.define(version: 2020_08_10_180417) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "review"
-    t.integer "user_id"
-    t.integer "beer_id"
+    t.integer "user_id", null: false
+    t.integer "beer_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["beer_id"], name: "index_reviews_on_beer_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,4 +53,7 @@ ActiveRecord::Schema.define(version: 2020_08_10_180417) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "beers", "breweries"
+  add_foreign_key "reviews", "beers"
+  add_foreign_key "reviews", "users"
 end
