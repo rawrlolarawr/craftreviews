bc4f635b7268f1f1e8f451c113cda790795f93aa
Christopher Chaison <christopherc0825@gmail.com>
2020-08-10 11:33:54 -0400

    adding views and controller actions for beers

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,11 +1,22 @@
 class BeersController < ApplicationController
+    before_action :set_beer!, only: [:show, :edit, :update, :destroy]
+
     def index
+        @beers = Beer.all
     end
     
     def new
+        @beer = Beer.new
     end
 
     def create
+        @beer = Beer.new(beer_params)
+
+        if @beer.save
+            redirect_to @beer
+        else
+            render :new
+        end
     end
 
     def show
@@ -15,8 +26,25 @@ class BeersController < ApplicationController
     end
 
     def update
+        if @beer.update(beer_params)
+            redirect_to @beer
+        else
+            render :edit
+        end
     end
 
     def destroy
+        @beer.destroy
+        redirect_to beers_url
+    end
+
+    private
+
+    def beer_params
+        params.require(:beer).permit(:name, :style, :abv)
+    end
+
+    def set_beer!
+        @beer = Beer.find_by(id: params[:id])
     end
 end
