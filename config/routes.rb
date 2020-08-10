5967380fca041b56e36772a0f9b54a8187aa0db2
Christopher Chaison <christopherc0825@gmail.com>
2020-08-10 14:35:58 -0400

    stubbing out reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
   
   resources :users, only: [:edit, :update, :show]
-  resources :breweries
+  resources :breweries do
+    resources :beers
+  end
   
   root to: "welcome#index"
 end
